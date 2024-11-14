1d1cf49bb2802b79a827429043d0c1c8730f2296
Jilen Sebamalainathan <jilen.sebamalainathan@efrei.net>
2024-11-14 11:36:39 +0100

    ajout contact

---
+++ view/contact.php
@@ -0,0 +1,22 @@
+<form id="contact" action="submit_form.php" method="POST">
+    <h2>Contact</h2>
+
+
+    <label for="nom">Nom :</label>
+    <input type="text" id="nom" name="nom" required>
+
+
+    <label for="prenom">Prénom :</label>
+    <input type="text" id="prenom" name="prenom" required>
+
+
+    <label for="email">Email :</label>
+    <input type="email" id="email" name="email" required>
+
+
+    <label for="message">Message :</label>
+    <textarea id="message" name="message" required></textarea>
+
+
+    <button type="submit">Envoyer</button>
+</form>
