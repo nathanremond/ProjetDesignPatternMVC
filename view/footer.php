a2f9b12e99b3177dc40516f22bcf7d7aac4a4b39
Jilen Sebamalainathan <jilen.sebamalainathan@efrei.net>
2024-11-14 11:41:58 +0100

    ajout footer avec css

---
+++ view/footer.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<style>
+    footer {
+    background-color: #333;
+    color: #fff;
+    padding: 15px;
+    text-align: center;
+    position: relative;
+    bottom: 0;
+    width: 100%;
+}
+
+.footer-container p {
+    margin: 10px 0;
+    font-size: 14px;
+}
+
+.footer-container a {
+    color: #4CAF50;
+    text-decoration: none;
+}
+
+.footer-container a:hover {
+    text-decoration: underline;
+}
+</style>
+
+<body>
+
+<footer>
+    <div class="footer-container">
+        <p><strong>Adresse :</strong> 2 rue paul vaillant couturier, 92000 , Nanterre, France</p>
+
+        <p><strong>Téléphone :</strong> 
+            <a href="tel:+33123456789">+33 1 01 01 01 01</a>
+        </p>
+
+        <p><strong>Instagram :</strong> 
+            <a href="https://www.instagram.com/SmartBike/" target="_blank">@smartbike</a>
+        </p>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14387.106459032631!2d2.3683840128825007!3d48.7873820517718!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e673e24e04a9c3%3A0xc55cb3e676f95321!2sEfrei!5e0!3m2!1sfr!2sfr!4v1731574279096!5m2!1sfr!2sfr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
+</footer>
+
+</body>
+</html>
